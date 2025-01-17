c9a3ccc144d334e6cf178bc769c7b1ad15e03531
Piotr Tomczyk <travis.blonded@gmail.com>
2025-01-17 08:15:11 +0100

    Continue tests

---
+++ pages/hello/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Hello World!
+        <div v-if="data">
+            {{ data }}
+        </div>
+        <div v-else>
+            No data
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    const { data } = await useFetch('/api/hello');
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button>
+      <NuxtLink to="/hello">
+        Hello
+      </NuxtLink>
+    </button>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>

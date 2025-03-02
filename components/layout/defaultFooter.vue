1215cba23e943eeea9295eb3797d086508dd9553
Piotr Tomczyk <travis.blonded@gmail.com>
2025-03-02 18:44:26 +0100

    Tests

---
+++ components/layout/defaultFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        <p>&copy; 2025</p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/layout/defaultHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>My Site</h1>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
